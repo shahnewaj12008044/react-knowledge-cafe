a444f08f6f014b1016c157db77e2bbed3f4e440e
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-01-28 23:49:53 +0600

    navbar done

---
+++ react-knowledge-cafe/src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import img from '../../images/profile.png'
+import './Header.css'
+
+const Header = () => {
+    
+    return (
+        <div className="nav-bar">
+            <h2>Coders Bd</h2>
+            <img src={img} alt="" />
+        </div>
+    );
+};
+
+export default Header;
