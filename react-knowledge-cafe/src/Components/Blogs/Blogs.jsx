0fd34f2f84fb1989a207fe5164f858ee9d7c61dc
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-01-29 23:17:47 +0600

    json created and blogs component created

---
+++ react-knowledge-cafe/src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Blogs.css'
+
+
+const Blogs = () => {
+    return (
+        <div className = "blogs-container">
+            <div className = 'single-blog'>
+                <h1>This is single Blog</h1> 
+            </div>
+            <div className='blogs-list'>
+                <h1>Blog list is here</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Blogs;
