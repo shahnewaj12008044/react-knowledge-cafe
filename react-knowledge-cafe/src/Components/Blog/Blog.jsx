28301c64cd50b70abba851e77e7e1e0fd9ef813f
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-02-02 15:32:43 +0600

    picture shown in ui

--- react-knowledge-cafe/src/Components/Blog/Blog.jsx
+++ react-knowledge-cafe/src/Components/Blog/Blog.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = ({blog}) => {
     console.log(blog)
     return (
         <div>
             
+            <img className='blog-image' src={blog.image} alt="" />
+            <p>name: {blog.Author_name}</p>
         </div>
     );
 };
